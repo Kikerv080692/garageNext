531bd4690594b348f24a4ff29513f1783d1a3a4e
Kikerv080692 <dp080692pva@gmail.com>
2025-05-15 15:03:49 +0200

    brakePage

--- src/pages/Brake/BrakePage.jsx
+++ src/pages/Brake/BrakePage.jsx
@@ -1,11 +1,20 @@
 "use client"
 import * as SC from './BrakePage.styled'
-
+import { useTranslation } from 'react-i18next'
 export default function BrakePage() {
-    
+    const {t} = useTranslation()
     return (
         <SC.WrapperBrakePage>
-    Brake
+            <h1>{t('brakePage.title')}</h1>
+            <h2>{t('brakePage.title2')}</h2>
+
+        <h3>{t('brakePage.title3')}</h3>
+        <ul>
+            <li>{t('brakePage.title4')}</li>
+            <li>{t('brakePage.title5')}</li>
+            <li>{t('brakePage.title6')}</li>
+        </ul>
+        <img src="/images/wheel.webp" alt="lap-top" />
     </SC.WrapperBrakePage>
     );
 }
--- src/pages/Brake/BrakePage.styled.js
+++ src/pages/Brake/BrakePage.styled.js
@@ -7,4 +7,28 @@ align-items: center;
 flex-direction: column;
 gap: 10px;
 margin-top: 50px;
+margin-left: 20px;
+margin-right: 20px;
+  word-break: break-word;
+  h1, h2, h3, p, li {
+    line-height: 1.6;
+    word-wrap: break-word;
+    overflow-wrap: break-word;
+    word-break: break-word;
+  }
+ul{
+    list-style: disc;
+    padding-left: 20px;
+    margin: 0;
+
+}
+  img {
+    margin-top: 10px;
+    margin-bottom: 20px;
+    width: 100%;
+    max-width: 380px;
+    height: auto;
+    border-radius: 8px;
+  }
+   
 `
